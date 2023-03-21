e48fb5460787166283ecb9591eb4518ad3aa9bbe
ho-sea <kinppq@naver.com>
2023-03-21 20:21:32 +0900

    0321_02_박세원_메뉴페이지 수정 및 기능추가

--- BOBJO/src/main/java/com/bobjo/reservation/db/ReservationDAO.java
+++ BOBJO/src/main/java/com/bobjo/reservation/db/ReservationDAO.java
@@ -3,12 +3,46 @@ package com.bobjo.reservation.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bobjo.menu.db.MenuDTO;
+import com.bobjo.utils.db.ConnectionManager;
 
 public class ReservationDAO {
 	private Connection con = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
 	private String sql = "";
-	
-	
+
+	/*
+	 * public List<ReservationDTO> insert(){ List<MenuDTO> menuList = new
+	 * ArrayList<MenuDTO>(); try {
+	 * 
+	 * con = ConnectionManager.getConnection();
+	 * sql="select * from bobjo_menu where store_no = ? "; pstmt =
+	 * con.prepareStatement(sql);
+	 * 
+	 * pstmt.setInt(1, store_no);
+	 * 
+	 * rs = pstmt.executeQuery();
+	 * 
+	 * 
+	 * while(rs.next()){ MenuDTO dto = new MenuDTO();
+	 * dto.setMenu_no(rs.getInt("menu_no"));
+	 * dto.setMenu_name(rs.getString("menu_name"));
+	 * dto.setMenu_img(rs.getString("menu_img"));
+	 * dto.setMenu_info(rs.getString("menu_info"));
+	 * dto.setPrice(rs.getInt("price")); dto.setStore_no(store_no);
+	 * dto.setMenu_category(rs.getString("menu_category")); menuList.add(dto); }
+	 * 
+	 * 
+	 * System.out.println("M : 디비 정보 가져오기 성공");
+	 * System.out.println(" DAO : "+menuList.size()); } catch (Exception e) {
+	 * System.out.println("M : 디비에 정보가 없습니다"); 알림 창띄우기 e.printStackTrace(); }
+	 * finally { ConnectionManager.closeConnection(rs, pstmt, con); }
+	 * 
+	 * return menuList; }
+	 */
+
 }
